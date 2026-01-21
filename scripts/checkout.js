255a4b4e08e5d26b845206ec252e3b252cf43e3a
Abinshibu <abinshibu994@gmail.com>
2026-01-21 22:37:32 +0530

    external library

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,14 @@ import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import {calculateCartQuantity} from '../data/cart.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'day').format('MMMM D');
+console.log(`Suggested delivery date: ${deliveryDate}`);
+console.log(hello());
+
 
 let cartSummaryHTML = '';
 
